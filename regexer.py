b8077d67c848e2a179c539f5c1fcf1659a7d9731
mahdi akhi <mahdiAkhy@gmail.com>
2022-04-15 02:52:37 +0430

    add prefixPatternGenerator method

--- regexer.py
+++ regexer.py
@@ -12,7 +12,7 @@ class Regexer:
         file = open(Path.cwd()/'PVC/Data/TXT/suffix.csv', encoding="utf-8")
         self.suffix = csv.reader(file)
 
-        file = open(Path.cwd()/'PVC/Data/TXT/prefix.csv', encoding="utf-8")
+        file = open(Path.cwd()/'PVC/Data/TXT/prefix.txt', encoding="utf-8")
         self.prefix = csv.reader(file)
 
     def compilePatterns(self, patterns):
@@ -22,7 +22,7 @@ class Regexer:
 
     def sffixPatternGenerator(self):
         patterns = []
-        for item in self.rules:
+        for item in self.suffix:
             space = '' if item[1] == 'a' else HALF_SPACE
             if item[2]!='':
                 pattern = r'(?<=('+item[2]+'))\s+(?=('+item[0]+'))'
@@ -33,4 +33,19 @@ class Regexer:
             replacement = space+r'\2\3'
             patterns.append(tuple([re.compile(pattern), replacement]))
         
+        return patterns
+
+    def prefixPatternGenerator(self):
+        patterns = []
+        for item in self.prefix:
+            space = '' if item[1] == 'a' else HALF_SPACE
+            if item[2] != '':
+                pattern = '(?<=('+item[0]+'))\s+(?=('+item[2]+'))'
+                replacement = '‌' if item[1] == 'a' else ''
+                patterns.append(tuple([re.compile(pattern), replacement]))
+
+            pattern = r'( )'+'('+item[0]+')'+r'( )'
+            replacement = r'\1\2'+ space
+            patterns.append(tuple([re.compile(pattern), replacement]))
+
         return patterns
