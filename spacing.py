e5335ede3484be66c8abea15e525a70ce2278b71
mahdi akhi <mahdiAkhy@gmail.com>
2022-04-14 17:57:38 +0430

    Add suffixFixer method to spacing class

--- spacing.py
+++ spacing.py
@@ -1,6 +1,12 @@
-import re
-
+from regexer import Regexer
 
 class Spacing:
     def __init__(self) -> None:
-        pass
+        self.regexer = Regexer()
+
+    def suffixFixer(self, text):
+
+        patterns = self.regexer.patternGenerator()
+        for pat, rep in patterns:
+            text = pat.sub(rep, text)      
+        return text
