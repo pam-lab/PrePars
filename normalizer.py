4044dbf341cffc2575a895548712de08329294e0
mahdi akhi <mahdiAkhy@gmail.com>
2022-04-14 15:11:56 +0430

    Move patternCompile method to regexer class

--- normalizer.py
+++ normalizer.py
@@ -1,29 +1,29 @@
-import re
-
+from regexer import Regexer
 
 class Normalizer:
+    def __init__(self) -> None:
+        self.regexer = Regexer()
 
     def characterRefine(self, text):
 
         character_refinement_patterns = [
             (r' {2,}', ' '),  # remove extra spaces
-        				(r'\n{3,}', '\n\n'),  # remove extra newlines
+        	(r'\n{3,}', '\n\n'),  # remove extra newlines
             (r'\u200c{2,}', '\u200c'),  # remove extra ZWNJs
-        				(r'[ـ\r]', ''),  # remove keshide, carriage returns
+        	(r'[ـ\r]', ''),  # remove keshide, carriage returns
             ('"([^\n"]+)"', r'«\1»'),  # replace quotation with gyoome
-        				('([\d+])\.([\d+])', r'\1٫\2'),  # replace dot with momayez
-        				(r' ?\.\.\.', ' …'),  # replace 3 dots
+        	('([\d+])\.([\d+])', r'\1٫\2'),  # replace dot with momayez
+        	(r' ?\.\.\.', ' …'),  # replace 3 dots
            	# remove FATHATAN, DAMMATAN, KASRATAN, FATHA, DAMMA, KASRA, SHADDA, SUKUN
            	('[\u064B\u064C\u064D\u064E\u064F\u0650\u0651\u0652]', ''),
         ]
+        
+        character_refinement_patterns = self.regexer.compilePatterns(character_refinement_patterns)
 
         translation_src, translation_dst = ' ىكي“”', ' یکی""'
         translation_src += '0123456789%,'
         translation_dst += '۰۱۲۳۴۵۶۷۸۹٪،'
         translations = self.makeTrans(translation_src, translation_dst)
-        character_refinement_patterns = self.compile_patterns(
-            character_refinement_patterns)
-
         text = text.translate(translations)
 
         for pattern, repl in character_refinement_patterns:
@@ -34,7 +34,7 @@ class Normalizer:
     def punctuationRefine(self, text):
 
         punc_after, punc_before = r'\.:!،؛؟»\]\)\}', r'«\[\(\{'
-        punctuation_spacing_patterns = self.compile_patterns([
+        punctuation_spacing_patterns = self.regexer.compilePatterns([
             # remove space before and after quotation
             ('" ([^\n"]+) "', r'"\1"'),
             (' ([' + punc_after + '])', r'\1'),  # remove space before
@@ -51,8 +51,5 @@ class Normalizer:
         for pattern, repl in punctuation_spacing_patterns:
             text = pattern.sub(repl, text)
         return text
-
-    def compile_patterns(self, patterns):
-        return [(re.compile(pattern), repl) for pattern, repl in patterns]
 
     def makeTrans(self, A, B): return dict((ord(a), b) for a, b in zip(A, B))
