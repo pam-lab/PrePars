63d331af201f4f48870e0c61535114dd8b9d6271
mahdi akhi <mahdiAkhy@gmail.com>
2022-04-15 11:01:26 +0430

    add fix method to spacing class

--- spacing.py
+++ spacing.py
@@ -1,7 +1,10 @@
-from regexer import Regexer
-import re
 from pathlib import Path
 import json
+import re
+
+from verb import verbProcessing
+from regexer import Regexer
+from normalizer import Normalizer
 
 class Spacing:
     def __init__(self) -> None:
@@ -25,10 +28,30 @@ class Spacing:
         
         file = open(Path.cwd()/'PVC/Data/TXT/replacement.json', encoding="utf-8")
         rep = json.load(file)
-        # print(rep)
 
         rep = dict((k, v) for k, v in rep.items())
         pattern = re.compile("|".join(rep.keys()))
         text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
 
         return text
+
+    def fix(self, text):
+
+        # normalizing the text
+        norm = Normalizer()
+        text = norm.normalize(text)
+
+        # fix the Verbs
+        verb=verbProcessing()
+        text = verb.fixVerbs(text)
+
+        # fix the suffixes
+        text = self.suffixFixer(text)
+
+        # fix the prefixes
+        text = self.prefixFixer(text)
+
+        # fix unregular Words
+        text = self.unregularWords(text)
+
+        return norm.normalize(text)
