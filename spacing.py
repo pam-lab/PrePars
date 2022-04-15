c88e9e1203d2d961a7654c7ec8683f61219c07da
mahdi akhi <mahdiAkhy@gmail.com>
2022-04-15 09:38:36 +0430

    add unregularWords method to spacing class

--- spacing.py
+++ spacing.py
@@ -1,4 +1,7 @@
 from regexer import Regexer
+import re
+from pathlib import Path
+import json
 
 class Spacing:
     def __init__(self) -> None:
@@ -16,4 +19,16 @@ class Spacing:
         patterns = self.regexer.prefixPatternGenerator()
         for pat, rep in patterns:
             text = pat.sub(rep, text)      
+        return text
+
+    def unregularWords(self, text):
+        
+        file = open(Path.cwd()/'PVC/Data/TXT/replacement.json', encoding="utf-8")
+        rep = json.load(file)
+        # print(rep)
+
+        rep = dict((k, v) for k, v in rep.items())
+        pattern = re.compile("|".join(rep.keys()))
+        text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+
         return text
