31385886a0cf8eea3e92733ef41582e5ec14a85a
Amir Pourmand <pourmand1376@gmail.com>
2022-07-04 06:55:03 +0430

    add readme.md

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name= "prepars",
     version= 0.1,
     long_description="",
     packages=setuptools.find_packages(),
+    py_modules=["prepars"],             # Name of the python package
+    package_dir={'':'src/prepars'},
 )
