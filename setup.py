43ecae93831e3ad316595e1fe25bd6452fb2ca4b
Amir Pourmand <pourmand1376@gmail.com>
2022-07-04 08:00:32 +0430

    change setup.py file'

--- setup.py
+++ setup.py
@@ -8,6 +8,4 @@ setuptools.setup(
     version= 0.1,
     long_description="",
     packages=setuptools.find_packages(),
-    py_modules=["prepars"],             # Name of the python package
-    package_dir={'':'src/prepars'},
 )
